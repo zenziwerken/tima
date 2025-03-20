d7b699c2c14dfd244c6244257fd1cd225512945a
Daniel Groß <daniel@zenziwerken.de>
2025-03-20 10:03:44 +0100

    Added a fallback in case mp3 could not be loaded

--- build_standalone.php
+++ build_standalone.php
@@ -2,17 +2,18 @@
 
 $html = file_get_contents('index.html');
 $filetypes = array(
-    'svg'      => 'data:image/svg+xml',
-    'woff2'    => 'data:font/woff2',
-    'mp3'      => 'data:audio/mp3',
-    'css'      => 'data:text/css',
-    'manifest' => 'data:application/manifest+json',
+    'svg'      => 'image/svg+xml',
+    'woff2'    => 'font/woff2',
+    'mp3'      => 'audio/mp3',
+    'css'      => 'text/css',
+    'manifest' => 'application/manifest+json',
+    'js'       => 'text/javascript',
 );
 
 preg_match_all('%"assets/(.+?)\.(.+?)"%sm', $html, $result, PREG_PATTERN_ORDER);
 foreach ($result[0] as $key => $filename) {
     $file = file_get_contents(__DIR__ . '/assets/' . $result[1][$key] . '.' . $result[2][$key]);
-    $base = '"'.$filetypes[$result[2][$key]].';base64,'.base64_encode($file).'"';
+    $base = '"data:'.$filetypes[$result[2][$key]].';base64,'.base64_encode($file).'"';
     $html = str_replace($filename, $base, $html);
 }
 file_put_contents('tima_standalone.html', $html);
